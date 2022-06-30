7ada8b68f9e80ae28b466338148ee3f3a4ffa5e3
Pietro Annobil <pietroannobil@Pietros-MacBook-Air.local>
2022-06-30 13:27:30 +0100

    num convertion from double to int

--- Main.java
+++ Main.java
@@ -1,5 +1,33 @@
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello World");
+
+    //variable
+    String myName = "Pietro";
+    Integer age = 33;
+    System.out.println(myName);
+    System.out.println(age);
+    //
+
+    System.out.println("---------");
+
+    //variable sum
+    int num1 = 10;
+    int num2 = 20;
+    int sum = num1+num2;
+    System.out.println("The sum of the two variable is: " + sum);
+    System.out.println("---------");
+    // 
+
+    //convert double to int
+    double num3 = 15.5;
+    int num3ToInt = (int) num3;
+    System.out.println(num3ToInt);
+    System.out.println("---------");
   }
 }
+
+
+
+// single comment
+/*multi line comment*/
+    
